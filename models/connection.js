7c8b80f87dfad2718999e7f369c1eca0b455d447
HrodWolfDev <102832449+HrodWolfS@users.noreply.github.com>
2025-11-02 11:56:30 +0100

    Fix: Increase MongoDB connection timeout for Vercel cold starts

--- models/connection.js
+++ models/connection.js
@@ -4,6 +4,9 @@ const connectionString = process.env.CONNECTION_STRING ||
   "mongodb+srv://stempfelrodolphe_db_user:5GvbqdnpgeWPS2ZT@cities.s7gpapg.mongodb.net/weatherapp";
 
 mongoose
-  .connect(connectionString, { connectTimeoutMS: 2000 })
+  .connect(connectionString, {
+    connectTimeoutMS: 30000,
+    serverSelectionTimeoutMS: 30000
+  })
   .then(() => console.log("Database connected"))
-  .catch((error) => console.error(error));
+  .catch((error) => console.error("MongoDB connection error:", error));
